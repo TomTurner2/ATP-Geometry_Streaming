683b9ddf9c86108a3e1f9548059e840cbf213bd4
Tom Turner <tom.turner2@googlemail.com>
2018-02-25 12:28:10 +0000

    Removed unused function

--- Assets/Classes/Voxels/Voxel.cs
+++ Assets/Classes/Voxels/Voxel.cs
@@ -20,25 +20,6 @@ public class Voxel
     }
 
 
-    public virtual Vector2[] GetFaceUVs(Direction _direction)
-    {
-        Vector2[] uvs = new Vector2[4];
-        TileTexture tile_texture_pos = GetTextureCoordsByDirection(_direction);
-
-        //Calculate uvs based on tile coordinates
-        uvs[0] = new Vector2(tile_texture_size * tile_texture_pos.x,
-            tile_texture_size * tile_texture_pos.y);
-        uvs[1] = new Vector2(tile_texture_size * tile_texture_pos.x,
-            tile_texture_size * tile_texture_pos.y + tile_texture_size);
-        uvs[2] = new Vector2(tile_texture_size * tile_texture_pos.x + tile_texture_size,
-            tile_texture_size * tile_texture_pos.y + tile_texture_size);
-        uvs[3] = new Vector2(tile_texture_size * tile_texture_pos.x + tile_texture_size,
-            tile_texture_size * tile_texture_pos.y);
-
-        return uvs;
-    }
-
-
     public virtual MeshInfo GetVoxelMeshInfo (Chunk _chunk, int _x, int _y, int _z, MeshInfo _mesh_info)
     {
         return MeshCreator.GenerateMesh(_chunk, _x, _y, _z, _mesh_info, this);
